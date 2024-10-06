88da884fbf12313931246e66cb69c17aa554154c
robynroby <robinsonmwaura254@gmail.com>
2024-10-06 15:12:24 +0300

    ui changes in homepage section

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,29 +1,48 @@
 import React from 'react';
-import { Container, Button } from 'react-bootstrap';
-import './style.css';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import Footer from './Footer';
+import './home.css';
 import NavigationBar from './NavigationBar';
 
-
 const HomePage = () => {
-
   return (
     <div className="starhost-homepage">
-      <NavigationBar/>
+      <NavigationBar />
       <Container className="starhost-main-content">
-        <h1 className="starhost-greeting-text">Hello, Joseph!</h1>
-        <p className="starhost-subheading-text">Here is what is happening with your property.</p>
-        <div className="starhost-property-placeholder-container">
-          <div className="starhost-placeholder-items-container">
-            <div className="starhost-placeholder-item-box"></div>
-            <div className="starhost-placeholder-item-box"></div>
-          </div>
-          <p className="starhost-property-message">Start by adding your first property</p>
-          <Button variant="primary" className="starhost-add-property-button">+ Add new property</Button>
-        </div>
+        <h1 className="starhost-greeting-text">Hello, joseph</h1>
+        <p className="starhost-subheading-text">Here's an overview of your property management.</p>
+        <Row className="g-4 justify-content-center">
+          <Col>
+            <Card className="starhost-info-card-home">
+              <Card.Body>
+                <Card.Title>Total Properties</Card.Title>
+                <Card.Text className="starhost-info-value">22</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="starhost-info-card-home">
+              <Card.Body>
+                <Card.Title>Total Bookings</Card.Title>
+                <Card.Text className="starhost-info-value">4</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col >
+            <Card className="starhost-info-card-home">
+              <Card.Body>
+                <Card.Title>Recent Activities</Card.Title>
+                <ul className="starhost-activity-list">
+                  <li>Joined on 27-06-2020</li>
+                  <li>New booking received for Property 2345678</li>
+                  <li>Updated amenities for Property 2334555</li>
+                </ul>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
-      <Footer/>
-      <Button variant="dark" className="starhost-feedback-button">Feedback</Button>
+      <Footer />
     </div>
   );
 };
